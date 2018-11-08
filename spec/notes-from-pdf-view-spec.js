a998428b5c98630a01549bd1eec7c14fae15a8a4
Andy Stuhl <andykstuhl@gmail.com>
2018-11-08 01:31:25 -0500

    basic package with paste handler

---
+++ spec/notes-from-pdf-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import NotesFromPdfView from '../lib/notes-from-pdf-view';
+
+describe('NotesFromPdfView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
