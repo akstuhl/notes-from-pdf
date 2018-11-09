97116f8aa8767cbf37bf9e8814604fab6793a131
Andy Stuhl <andykstuhl@gmail.com>
2018-11-09 11:35:38 -0500

    add page numbers list item feature

--- spec/notes-from-pdf-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import NotesFromPdfView from '../lib/notes-from-pdf-view';
-
-describe('NotesFromPdfView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
